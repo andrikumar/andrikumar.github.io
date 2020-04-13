85c29cf0d5f4f181a82836ec605fde7fa08f3bd6
Andri Kumar <46429980+andrikumar@users.noreply.github.com>
2020-04-13 02:56:14 -0400

    Update script.js

--- april9hw/script.js
+++ april9hw/script.js
@@ -1,34 +1,48 @@
 const app = {
 
 initialize: function () {
+    //listener one
       $('.click').dblclick(function(){
         app.leave();
   });
+      //listener two
   $('.impatient').mouseover(function(){
      app.highlight();
+     app.remove();
+});
+//listener four
+$('.heading').click(function(){
+   app.hideIt;
 });
 
-
-
 },
-
+//basic function 1!
 leave: function() {
    $('.calm').text("please calm down");
    $( ".click" ).remove();
     $('.impatient').text("has anyone every told you that you are impatient");
 },
-
+//basic function 2!
 highlight: function() {
   $(".impatient").mouseover(function(){
     $(".impatient").css("background-color", "yellow");
     $("#sectionTwo").delay(4000).fadeIn();
     $('.lastPoint').text("*eye roll*");
+
   });
   $(".impatient").mouseout(function(){
     $(".impatient").css("background-color", "white");
   });
+},
+//basic function 3!
+remove: function() {
+//listener three
+  $('.lastPoint').click(function() {
+$( ".lastPoint" ).hide();
+});
+},
 
-
-
+hideIt: function() {
+   $( ".heading" ).hide();
 }
 }
