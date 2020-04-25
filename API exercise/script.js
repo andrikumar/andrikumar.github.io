15dd1ea8d1fc3a6f0ed0d1d2eb69cbf6fc71ce09
Andri Kumar <46429980+andrikumar@users.noreply.github.com>
2020-04-25 14:06:27 -0400

    Add files via upload

--- API exercise/script.js
+++ API exercise/script.js
@@ -3,8 +3,24 @@ const app = {
     $('.prompt').click(function () {
       app.rover();
       app.toDo();
+      $("button").remove();
+      app.header();
+      app.ending();
+      app.collectDate();
     });
+  
+  },
 
+  header: function() {
+    //placing text for the heading/introduction
+    $('.firstLine').text("its going to be okay");
+    $('.secondLIne').text("did you know that Curiosity, the Mars Rover, is the only rover still activethe others have lost contact");
+  },
+
+  ending: function() {
+    //placing text for the ending
+    $('.thirdLine').text("being in the company of just your own presence can be a wonderful path to take");
+    $('.fourthLine').text("or learn what Curiosity is doing today");
   },
 
   rover: function() {
@@ -12,10 +28,7 @@ const app = {
       url: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=dRIKo0fwLmL6Wn73dUxAwbCekkgChgWYcALn3pTX',
       dataType: 'json',
       success: data => {
-        var i;
-      for (i = 0; i < 10; i++) {
-        app.displayImages(data.photos[i].img_src);
-        }
+           app.displayImages(data);
       }
     })
   },
@@ -32,22 +45,41 @@ const app = {
   },
 
   displayImages: function (data) {
+      //list of poetry
+      var text = '{ "poems" : [' +
+      '{ "lineOne":"John" , "lineTwo":"Doe" , "lineThree":"Hola" },' +
+      '{ "lineOne":"Anna" , "lineTwo":"Smith" },' +
+      '{ "lineOne":"Peter" , "lineTwo":"Jones" } ]}';
+      var obj = JSON.parse(text);
 
+      //selecting a random poem to display
+      var r = Math.floor(Math.random() * 3) + 1;
+      console.log(r);
+      const values = Object.values(obj.poems[r])
 
-      $.getJSON('data.json', function(jd) {
-console.log(jd.employees[1]);
-      });
-//putting poetry into HTML
+      //going through the lines of a poem along with the images
+      for (i = 0; i < 4;  i++) {
+            //trying to remove every other image from the array 
 
-//var obj = JSON.parse(text);
-
-    //putting image into HTML
-    const img = $(`<div class="roverImage"><img src=${data} /></div>`);
-      $('.photosContainer').append(img);
-      //$('.photosContainer').append(obj.employees[1].firstName);
-  },
+            const img = $(`<div class="roverImage"><img src=${data.photos[i].img_src} /></div>`);
+            $('.photosContainer').append(img);
+              //breaking up the lines of poetry
+              console.log(values[i]);
+              $('.photosContainer').append(values[i]);
+      
+  }
+},
 
   listThings: function (data) {
-    $('.todoContainer').html(data);
+    //providing a task to do in isolation
+    $('.todoContainer').html("today maybe " + data);
   },
+
+  collectDate: function() {
+    var today = new Date();
+    var date = today.getDate();
+    console.log(date);
+  },
+
 }
+
